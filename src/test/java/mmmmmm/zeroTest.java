4c5400e9e1868bc7c1513b67a9bd6298c90e9381
subhash <subhashhogale5>
2020-10-04 11:42:17 +0530

    this my first commit

---
+++ src/test/java/mmmmmm/zeroTest.java
@@ -0,0 +1,17 @@
+package mmmmmm;
+
+import org.testng.annotations.Test;
+
+public class zeroTest {
+	@Test
+	public void tc1()
+	
+	{
+		System.out.println("hiii this subhash");
+	}
+	@Test
+	public void loginPage() {
+		System.out.println("go to hell");
+	}
+
+}
